d729bff187370b1a267e353a091480bf7c3f6f61
fauzansofraaa <150792015+fauzansofraa@users.noreply.github.com>
2026-04-15 20:57:38 +0700

    Revisi Main.c

--- main.c
+++ main.c
@@ -1,60 +1,29 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include "auth.h"
-#include "menu.h"
-#include "file_handler.h"
+#include "login.h"
+#include "admin.h"
+#include "user.h"
 
 int main(int argc, char *argv[]) {
-
-    // Cek argumen
     if (argc != 3) {
-        printf("Usage: %s <username> <password>\n", argv[0]);
+        printf("Pakai: ./app username password\n");
         return 1;
     }
 
-    
-    inisialisasiSemuaFile();
+    char role[10];
 
-    char *username = argv[1];
-    char *password = argv[2];
-
-    // 3. Login (ambil dari akun.txt)
-    int role = login(username, password);
-
-    if (role == -1) {
+    if (!loginUser(argv[1], argv[2], role)) {
         printf("Login gagal!\n");
         return 1;
     }
 
-    printf("\nLogin berhasil sebagai %s\n", username);
+    printf("Login berhasil sebagai %s\n", role);
 
-    
-    if (role == 1) {
-        
-        int pilihan;
-        do {
-            printf("\n=== MENU ADMIN ===\n");
-            printf("1. Kelola Alat\n");
-            printf("2. Tambah Akun (Admin bisa nambah user)\n");
-            printf("0. Logout\n");
-            printf("Pilih: ");
-            scanf("%d", &pilihan);
-
-            if (pilihan == 1) {
-                menuAdmin(); 
-            }
-            else if (pilihan == 2) {
-                tambahAkun();
-            }
-
-        } while (pilihan != 0);
-
-    } else if (role == 2) {
-        
-        menuUser(username);
+    if (strcmp(role, "admin") == 0) {
+        menuAdmin();
+    } else {
+        menuUser(argv[1]);
     }
 
-    printf("\nKeluar dari sistem.\n");
     return 0;
 }
