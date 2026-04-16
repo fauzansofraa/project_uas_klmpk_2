106819f5a46cda85ebe73c5ee78e6ae0f25eed43
Habib200706 <habibabrar527@gmail.com>
2026-04-16 13:01:01 +0700

    membuat buat login user

---
+++ login.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+#include "login.h"
+
+int loginUser(char *user, char *pass, char *role) {
+    FILE *f = fopen("data/akun.txt", "r");
+    if (!f) return 0;
+
+    char u[50], p[50], r[10];
