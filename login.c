8dddd9e0c2565def1447ab05087dc5456d4bcd22
Habib200706 <habibabrar527@gmail.com>
2026-04-16 13:12:00 +0700

    membuat buat login user

--- login.c
+++ login.c
@@ -7,15 +7,3 @@ int loginUser(char *user, char *pass, char *role) {
     if (!f) return 0;
 
     char u[50], p[50], r[10];
-
-    while (fscanf(f, "%s %s %s", u, p, r) != EOF) {
-        if (strcmp(u, user) == 0 && strcmp(p, pass) == 0) {
-            strcpy(role, r);
-            fclose(f);
-            return 1;
-        }
-    }
-
-    fclose(f);
-    return 0;
-}
