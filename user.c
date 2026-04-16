526c4648ae973c56181364913c8f7c69ca939c85
faizazamra07 <faizazamra2007@gmail.com>
2026-04-16 13:00:52 +0700

    membuat fungsi lihat pinjam

--- user.c
+++ user.c
@@ -28,3 +28,22 @@ void pinjam(char *user) {
 
     printf("Berhasil pinjam %s\n", namaAlat);
 }
+void lihatPinjam(char *user) {
+    FILE *f = fopen("data/pinjam.txt", "r");
+
+    char u[50], alat[50];
+    int found = 0;
+
+    printf("\n=== PINJAMAN ANDA ===\n");
+
+    while (fscanf(f, "%[^|]|%[^\n]\n", u, alat) != EOF) {
+        if (strcmp(u, user) == 0) {
+            printf("- %s\n", alat);
+            found = 1;
+        }
+    }
+
+    if (!found) printf("Belum ada pinjaman\n");
+
+    fclose(f);
+}
